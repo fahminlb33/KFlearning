780a4f516b560053b76dafe51af3b5ecea10bc2e
fahminlb33 <fahminlb33@gmail.com>
2020-06-10 21:38:49 +0700

    Change port to 21002

--- src/KFserver/KFserver.c
+++ src/KFserver/KFserver.c
@@ -4,7 +4,7 @@
 
 #define BUFSIZE 4096
 
-static const char* s_http_port = "8000";
+static const char* s_http_port = "21002";
 static struct mg_serve_http_opts s_http_server_opts;
 
 static void ev_handler(struct mg_connection* nc, int ev, void* p)
