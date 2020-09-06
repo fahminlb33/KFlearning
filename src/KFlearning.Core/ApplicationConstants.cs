285c738b9e429fedee75b9f0ec743774e814d498
fahminlb33 <fahminlb33@gmail.com>
2020-09-06 12:11:24 +0700

    Revert basck to include secret in source

--- src/KFlearning.Core/ApplicationConstants.cs
+++ src/KFlearning.Core/ApplicationConstants.cs
@@ -2,7 +2,7 @@
 {
     public class ApplicationConstants
 	{
-		public const string ApiKey = "KFLEARNING_API_KEY";
+		public const string ApiKey = "6c3dcaf1a0b1bba771dd9fb3f3981cf1262d75cb313245d7fa173fe93f4221b4";
 		public const string Codename = "Hardbass";
 	}
 }
