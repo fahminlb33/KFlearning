6e941271d15fb32dae7cd7c121c91bebb0eef761
fahminlb33 <fahminlb33@gmail.com>
2021-02-28 11:01:49 +0700

    Refactor entity

---
+++ src/KFlearning/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using KFlearning.Core.Extensions;
+using KFlearning.TemplateProvider;
+using Newtonsoft.Json;
+using System;
+
+namespace KFlearning.Models
+{
+    public class Project
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastOpenAt { get; set; }
+
+        [JsonIgnore]
+        public ITemplateProvider Template { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({PathHelpers.TrimLongText(Path)})";
+        }
+    }
+}
