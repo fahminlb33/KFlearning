a6e5b99f867cc455a8ff6f4cd4d521316cca51d8
fahminlb33 <fahminlb33@gmail.com>
2021-02-28 11:04:41 +0700

    Use resource file instead of constants

--- src/KFlearning.Core/ApplicationConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KFlearning.Core
-{
-    public class ApplicationConstants
-	{
-		public const string ApiKey = "6c3dcaf1a0b1bba771dd9fb3f3981cf1262d75cb313245d7fa173fe93f4221b4";
-		public const string Codename = "Hardbass";
-	}
-}
