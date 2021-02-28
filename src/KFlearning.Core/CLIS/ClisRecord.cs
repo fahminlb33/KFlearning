d9e352f04854eed63d1e4f89d0ce0a06a1e700f2
fahminlb33 <fahminlb33@gmail.com>
2021-02-28 11:03:17 +0700

    Remove CLIS service

--- src/KFlearning.Core/CLIS/ClisRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KFlearning.Core.CLIS
-{
-    public class ClisRecord
-    {
-        public string Npm { get; set; }
-        public string Name { get; set; }
-        public double Score { get; set; }
-    }
-}
