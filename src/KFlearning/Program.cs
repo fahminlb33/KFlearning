1502ae65476724975b555d01326c09147b95b71d
fahminlb33 <fahminlb33@gmail.com>
2021-03-08 08:40:11 +0700

    Bump version

--- src/KFlearning/Program.cs
+++ src/KFlearning/Program.cs
@@ -24,51 +24,64 @@ namespace KFlearning
         [STAThread]
         static void Main(string[] args)
         {
-            using (var mutex = new Mutex(true, MutexName))
+            try
             {
-                if (!mutex.WaitOne(MutexTimeout))
+                using (var mutex = new Mutex(true, MutexName))
                 {
-                    MessageBox.Show(Resources.SingleInstanceMessage, Resources.AppName,
-                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                    return;
+                    if (!mutex.WaitOne(MutexTimeout))
+                    {
+                        MessageBox.Show(Resources.SingleInstanceMessage, Resources.AppName,
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+
+                    // install services
+                    NLog.GlobalDiagnosticsContext.Set("logDirectory", PathHelpers.GetLogPath());
+                    Container.Install(new KFlearningModulesInstaller());
+                    _logger = Container.Resolve<ILogger>();
+
+                    // find vscode
+                    var path = Container.Resolve<IPathManager>();
+                    if (!path.IsVscodeInstalled)
+                    {
+                        _logger.Debug("Visual Studio Code not found");
+                        MessageBox.Show(Resources.VscodeNotInstalled, Resources.AppName, MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+                        return;
+                    }
+
+                    // find mingw
+                    if (!path.IsKfMingwInstalled)
+                    {
+                        _logger.Debug("KF-MinGW not found");
+                        MessageBox.Show(Resources.KfmingwNotInstalled, Resources.AppName, MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+                        return;
+                    }
+
+                    // enable TLS
+                    _logger.Debug("Enabling TLS support");
+                    ApiHelpers.EnableTls();
+
+                    // app exit handler
+                    Application.ApplicationExit += Application_ApplicationExit;
+
+                    // bootstrapper
+                    _logger.Debug("Bootstrapping application");
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(Container.Resolve<KFlearningApplicationContext>());
                 }
-
-                // install services
-                NLog.GlobalDiagnosticsContext.Set("logDirectory", PathHelpers.GetLogPath());
-                Container.Install(new KFlearningModulesInstaller());
-                _logger = Container.Resolve<ILogger>();
-
-                // find vscode
-                var path = Container.Resolve<IPathManager>();
-                if (!path.IsVscodeInstalled)
-                {
-                    _logger.Debug("Visual Studio Code not found");
-                    MessageBox.Show(Resources.VscodeNotInstalled, Resources.AppName, MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
-                    return;
-                }
-
-                // find mingw
-                if (!path.IsKfMingwInstalled)
-                {
-                    _logger.Debug("KF-MinGW not found");
-                    MessageBox.Show(Resources.KfmingwNotInstalled, Resources.AppName, MessageBoxButtons.OK,
-                        MessageBoxIcon.Exclamation);
-                    return;
-                }
-
-                // enable TLS
-                _logger.Debug("Enabling TLS support");
-                ApiHelpers.EnableTls();
-
-                // app exit handler
-                Application.ApplicationExit += Application_ApplicationExit;
-
-                // bootstrapper
-                _logger.Debug("Bootstrapping application");
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(Container.Resolve<KFlearningApplicationContext>());
+            }
+            catch (Exception e)
+            {
+                _logger.Fatal("Application shutdown unexpectedly", e);
+                MessageBox.Show("Aplikasi mengalami crash dan harus ditutup. Harap laporkan kepada asprak.",
+                    Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
             }
         }
 
@@ -84,10 +97,6 @@ namespace KFlearning
             catch (Exception ex)
             {
                 _logger.Error("Cannot save persistence", ex);
-            }
-            finally
-            {
-                NLog.LogManager.Shutdown();
             }
 
             Container.Dispose();
